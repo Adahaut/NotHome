3213f1c09f5f1d8ffee9a67be26e694c01ddba4f
Elvin-04 <e.berger@lahorde.school>
2024-05-16 16:07:52 +0100

    Update NetworkLobbyManager.cs

--- NotHome/Assets/Scripts/Multiplayer/NetworkLobbyManager.cs
+++ NotHome/Assets/Scripts/Multiplayer/NetworkLobbyManager.cs
@@ -150,7 +150,7 @@ public class NetworkLobbyManager : NetworkManager
     {
         if(SceneManager.GetActiveScene().path == menuScene && newSceneName.StartsWith("Scene_Map"))
         {
-            for(int i = _roomPlayers.Count - 1; i >= 0; i--)
+            for(int i = _roomPlayers.Count; i >= 0; i--)
             {
                 var conn = _roomPlayers[i].connectionToClient;
                 var gamePlayerInstance = Instantiate(_gamePlayerPrefab);
