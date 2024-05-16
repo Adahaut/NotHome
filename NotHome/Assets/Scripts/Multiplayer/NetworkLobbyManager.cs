3481ba7b73e791ed8ec4415f912e9f2938b41e71
Elvin-04 <e.berger@lahorde.school>
2024-05-16 14:57:45 +0100

    update

--- NotHome/Assets/Scripts/Multiplayer/NetworkLobbyManager.cs
+++ NotHome/Assets/Scripts/Multiplayer/NetworkLobbyManager.cs
@@ -3,7 +3,6 @@ using Steamworks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
