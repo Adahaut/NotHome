1e563e0e44257a5687d9be1e207ca4c54ff17114
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-21 17:23:53 +0100

    a

--- NotHome/Assets/ProximityVoiceChat.cs
+++ NotHome/Assets/ProximityVoiceChat.cs
@@ -63,14 +63,14 @@ public class ProximityVoiceChat : NetworkBehaviour
     void Target_PlaySound(NetworkConnection conn, byte[] destBuffer, uint bytesWritten, float volume)
     {
         Debug.Log("Target");
-        byte[] destBuffer2 = new byte[44100 * 2];
+        byte[] destBuffer2 = new byte[22050 * 2];
         uint bytesWritten2;
-        EVoiceResult ret = SteamUser.DecompressVoice(destBuffer, bytesWritten, destBuffer2, (uint)destBuffer2.Length, out bytesWritten2, 44100);
+        EVoiceResult ret = SteamUser.DecompressVoice(destBuffer, bytesWritten, destBuffer2, (uint)destBuffer2.Length, out bytesWritten2, 22050);
         if (ret == EVoiceResult.k_EVoiceResultOK && bytesWritten2 > 0)
         {
-            audioSource.clip = AudioClip.Create(UnityEngine.Random.Range(100, 1000000).ToString(), 44100, 1, 44100, false);
+            audioSource.clip = AudioClip.Create(UnityEngine.Random.Range(100, 1000000).ToString(), 22050, 1, 22050, false);
 
-            float[] test = new float[44100];
+            float[] test = new float[22050];
             for (int i = 0; i < test.Length; i++)
             {
                 test[i] = (short)(destBuffer2[i * 2] | destBuffer2[i * 2 + 1] << 8) / 32768.0f;
