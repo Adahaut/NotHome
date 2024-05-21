7cbb74f7d21bc4b9a720d29502b8853b1e76c709
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-21 11:10:48 +0100

    attempt

--- NotHome/Assets/ProximityVoiceChat.cs
+++ NotHome/Assets/ProximityVoiceChat.cs
@@ -17,6 +17,7 @@ public class ProximityVoiceChat : NetworkBehaviour
     {
         if (isOwned)
         {
+            SteamUser.StartVoiceRecording();
             voiceDataBuffer = new byte[voiceBufferSize];
         }
     }
@@ -33,7 +34,7 @@ public class ProximityVoiceChat : NetworkBehaviour
     {
         uint bytesWritten = 0;
         EVoiceResult result = SteamUser.GetVoice(true, voiceDataBuffer, voiceBufferSize, out bytesWritten);
-        test.text = "caca";
+        
         if (result == EVoiceResult.k_EVoiceResultOK && bytesWritten > 0)
         {
             test.text = bytesWritten.ToString();
