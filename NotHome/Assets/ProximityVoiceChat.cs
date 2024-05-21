43d30b3ec478bdb612d2c69ce56d23c24fe895cc
Adahaut <adao.vo.t@gmail.com>
2024-05-21 14:07:53 +0200

    Update ProximityVoiceChat.cs

--- NotHome/Assets/ProximityVoiceChat.cs
+++ NotHome/Assets/ProximityVoiceChat.cs
@@ -69,8 +69,12 @@ public class ProximityVoiceChat : NetworkBehaviour
 
         if (voiceResult == EVoiceResult.k_EVoiceResultOK && bytesWritten > 0)
         {
-            audioSource.clip = AudioClip.Create(UnityEngine.Random.Range(100, 1000000).ToString(), 44100, 1, 44100, false);
             test.text = audioSource.clip.name; 
+            if (audioSource == null)
+            {
+                test.text = "elvin ta mère la pute !";
+            }
+            audioSource.clip = AudioClip.Create(UnityEngine.Random.Range(100, 1000000).ToString(), 44100, 1, 44100, false);
             float[] testa = new float[44100];
             for (int i = 0; i < testa.Length; ++i)
             {
