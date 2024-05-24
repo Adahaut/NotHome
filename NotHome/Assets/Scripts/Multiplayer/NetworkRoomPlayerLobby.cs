3b7908c59001acff3826763c84958d2c0ab95fb3
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-24 14:57:55 +0200

    test

--- NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
+++ NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
@@ -141,6 +141,11 @@ public class NetworkRoomPlayerLobby : NetworkBehaviour
 
             _playerImages[i].texture = Room._roomPlayers[i]._displayImage;
 
+            if (Room._roomPlayers[i] == this && isOwned)
+            {
+                _leaveKickButtons[i].gameObject.SetActive(true);
+                _leaveKickButtons[i].GetComponentInChildren<TMP_Text>().text = "Leave";
+            }
 
             //Button Kick / Leave
             //if (Room._roomPlayers[i]._isLeader && _isLeader || Room._roomPlayers[i] == this)
