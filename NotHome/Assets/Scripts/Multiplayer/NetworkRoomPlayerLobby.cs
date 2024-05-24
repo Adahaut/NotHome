2014f5635bb7b6c90148cdac39414bf048b760a4
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-24 17:12:20 +0200

    Update NetworkRoomPlayerLobby.cs

--- NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
+++ NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
@@ -177,6 +177,10 @@ public class NetworkRoomPlayerLobby : NetworkBehaviour
         }
 
         Room._roomPlayers.Remove(this);
+        for (int i = 0; i < Room._roomPlayers.Count; i++)
+        {
+            Room._roomPlayers[i].connectionToClient.Disconnect();
+        }
 
         if (Room._roomPlayers.Count == 0)
         {
