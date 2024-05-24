041fc357646a18b6c970ae744c3d45cb9590e3d4
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-24 12:06:35 +0200

    Update PlayerCameraManager.cs

--- NotHome/Assets/PlayerCameraManager.cs
+++ NotHome/Assets/PlayerCameraManager.cs
@@ -11,7 +11,12 @@ public class PlayerCameraManager : NetworkBehaviour
     [SerializeField] private Camera playerRenderCamera;
 
 
-    private int index;
+    //private int index;
+
+    private static int nextPlayerId = 0;
+
+    [SyncVar(hook = nameof(OnPlayerIdChanged))]
+    private int playerId = -1; // Initialize to -1 to detect uninitialized state
 
     public TMP_Text test;
 
@@ -19,17 +24,44 @@ public class PlayerCameraManager : NetworkBehaviour
 
     public override void OnStartClient()
     {
+        //base.OnStartClient();
+
+        //if (isOwned)
+        //{
+        //    test.gameObject.SetActive(true);
+        //    test.text = netId.ToString();
+
+        //    if (playerRenderCamera != null)
+        //    {
+        //        playerRenderCamera.targetTexture = renderTextures[index];
+        //        CmdSetupCameraDisplay(index, renderTextures[index].name);
+        //    }
+        //}
+
         base.OnStartClient();
 
         if (isOwned)
         {
-            test.gameObject.SetActive(true);
-            test.text = netId.ToString();
+            CmdRequestPlayerId();
+        }
+    }
 
+    [Command]
+    private void CmdRequestPlayerId(NetworkConnectionToClient sender = null)
+    {
+        playerId = nextPlayerId++;
+    }
+
+    private void OnPlayerIdChanged(int oldPlayerId, int newPlayerId)
+    {
+        if (isOwned)
+        {
+            test.gameObject.SetActive(true);
+            test.text = newPlayerId.ToString();
             if (playerRenderCamera != null)
             {
-                playerRenderCamera.targetTexture = renderTextures[index];
-                CmdSetupCameraDisplay(index, renderTextures[index].name);
+                playerRenderCamera.targetTexture = renderTextures[newPlayerId];
+                CmdSetupCameraDisplay(newPlayerId, renderTextures[newPlayerId].name);
             }
         }
     }
