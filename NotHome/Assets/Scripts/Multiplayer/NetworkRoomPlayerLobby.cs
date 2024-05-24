7016a7759dae29d006012b0ce8a721fe4fce34c8
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-05-24 16:57:18 +0200

    Update NetworkRoomPlayerLobby.cs

--- NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
+++ NotHome/Assets/Scripts/Multiplayer/NetworkRoomPlayerLobby.cs
@@ -199,7 +199,6 @@ public class NetworkRoomPlayerLobby : NetworkBehaviour
         //Room.NotifyPlayersOfReadyState();
     }
 
-    [Client]
     private void BackToMainMenu()
     {
         var mainMenu = GameObject.Find("UI_MainMenu").GetComponent<MainMenu>();
