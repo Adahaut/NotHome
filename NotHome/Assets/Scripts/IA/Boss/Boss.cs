565370d1e8f152df720835d37fffbc41482d272d
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-05-27 15:09:39 +0200

    boss

--- NotHome/Assets/Scripts/IA/Boss/Boss.cs
+++ NotHome/Assets/Scripts/IA/Boss/Boss.cs
@@ -17,6 +17,7 @@ public class Boss : MonoBehaviour
     private bool _isStunned;
     private Transform _transform;
     private Transform _qgTransform;
+    private Vector3 _chargeDirection;
 
     private void Start()
     {
@@ -31,31 +32,39 @@ public class Boss : MonoBehaviour
     {
         if (_isStunned)
         {
+            //_agent.ResetPath();
             return;
         }
 
         _distanceToQg = Vector3.Distance(_transform.position, _qgTransform.position);
         if (!_isCharging && _distanceToQg <= _chargingDistance)
         {
+            Debug.Log("supposed to charge");
             StartCoroutine(ChargeAttack());
         }
     }
 
     IEnumerator ChargeAttack()
     {
+        Debug.Log("WaitBeforeCharging");
         _isCharging = true;
         _agent.ResetPath();
         yield return new WaitForSeconds(_chargeTime);
 
-        _agent.speed = _chargeSpeed;
-        _agent.SetDestination(_qg.transform.position);
+        Debug.Log("Charge");
+        _chargeDirection = (_qgTransform.position - _transform.position).normalized;
 
-        while (!_isStunned && Vector3.Distance(_transform.position, _qg.transform.position) > _agent.stoppingDistance)
+        float chargeStartTime = Time.time;
+        while (!_isStunned && Vector3.Distance(_transform.position, _qgTransform.position) > _agent.stoppingDistance)
         {
+            float elapsedTime = Time.time - chargeStartTime;
+            Vector3 nextPosition = _transform.position + _chargeDirection * _chargeSpeed * Time.deltaTime;
+            _agent.Warp(nextPosition);
             yield return null;
         }
 
         _isCharging = false;
+        _agent.SetDestination(_qgTransform.position);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -74,12 +83,25 @@ public class Boss : MonoBehaviour
         _isCharging = false;
         _agent.ResetPath();
 
-        Vector3 _knockbackDirection = (_transform.position - _qgTransform.position).normalized;
-        Vector3 _knockbackPosition = _transform.position + _knockbackDirection * _knockbackDistance;
-        _agent.Warp(_knockbackPosition); //teleport
+        Vector3 knockbackDirection = (_transform.position - _qgTransform.position).normalized;
+        Vector3 knockbackPosition = _transform.position + knockbackDirection * _knockbackDistance;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(knockbackPosition, out hit, _knockbackDistance, NavMesh.AllAreas))
+        {
+            knockbackPosition = hit.position;
+        }
+        else
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * 60f;
+            randomDirection += _transform.position;
+            NavMesh.SamplePosition(randomDirection, out hit, 60f, NavMesh.AllAreas);
+            knockbackPosition = hit.position;
+        }
 
         yield return new WaitForSeconds(_stunDuration);
-        
+        _agent.Warp(knockbackPosition);
+
         _isStunned = false;
         _agent.SetDestination(_qgTransform.position);
     }
