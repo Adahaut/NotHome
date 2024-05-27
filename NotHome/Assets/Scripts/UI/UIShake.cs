4853e53fe8dec9c1cb8c9b9b7f0379d0f6b7be0c
Adahaut <adao.vo.t@gmail.com>
2024-05-27 10:42:24 +0200

    fix

--- NotHome/Assets/Scripts/UI/UIShake.cs
+++ NotHome/Assets/Scripts/UI/UIShake.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class UIShake : MonoBehaviour
 {
+    public Transform _playerTransform;
     public Transform _playerCamera;
     public float _driftAmount = 0.1f;
     public float _positionDriftAmount = 0.1f;
@@ -26,15 +27,6 @@ public class UIShake : MonoBehaviour
         if (_cameraRotationDelta.y > 180) _cameraRotationDelta.y -= 360;
         if (_cameraRotationDelta.y < -180) _cameraRotationDelta.y += 360;
 
-        print("camera rotation : " + _cameraRotationDelta);
-
-        if (_cameraRotationDelta.x <= 3f && _cameraRotationDelta.y <= 3f)
-        {
-            print("0");
-            return Vector3.zero;
-        }
-            
-
         return  new Vector3(
             -_cameraRotationDelta.y * _driftAmount,
             _cameraRotationDelta.x * _driftAmount,
@@ -42,23 +34,13 @@ public class UIShake : MonoBehaviour
         );
     }
 
-    private bool DistanceBetweenPositionAndTargetPos(Vector3 _position, Vector3 _targetPosition)
-    {
-        return _targetPosition.x < _position.x + 0.5f && _targetPosition.x > _position.x - 0.5f
-            && _targetPosition.y < _position.y + 0.5f && _targetPosition.y > _position.y - 0.5f
-            && _targetPosition.z < _position.z + 0.5f && _targetPosition.z > _position.z - 0.5f;
-    }
-
     void Update()
     {
         Vector3 _driftOffset = UIDriftWithCameraRotation();
-        print("drift offset : " + _driftOffset);
 
-        Vector3 _targetPosition =  _initialPosition + _driftOffset;
-        print("target position : " + _targetPosition);
+        Vector3 _targetPosition = _initialPosition + _driftOffset;
 
-        transform.localPosition = DistanceBetweenPositionAndTargetPos(transform.localPosition, _targetPosition) ? 
-            Vector3.zero : Vector3.Lerp(transform.localPosition, _targetPosition, Time.deltaTime * _smoothSpeed);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _targetPosition, Time.deltaTime * _smoothSpeed);
 
         _previousCameraRotation = _playerCamera.eulerAngles;
     }
