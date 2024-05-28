62502622d4f86b5b6f7e78644271f33ec5755fa0
Adahaut <adao.vo.t@gmail.com>
2024-05-28 12:01:49 +0200

    proute

--- NotHome/Assets/PlayerFieldUI.cs
+++ NotHome/Assets/PlayerFieldUI.cs
@@ -6,22 +6,29 @@ using UnityEngine;
 public class PlayerFieldUI : NetworkBehaviour
 {
     private GameObject canvas;
-
+    private GameObject newCanvas;
     private void OnEnable()
     {
         if(isOwned && !NewFieldManager.instance._panelOpen)
         {
             canvas = NewFieldManager.instance.fieldPlayerCanvas;
-            canvas.SetActive(true);
+            newCanvas = Instantiate(canvas, this.transform);
+            newCanvas.SetActive(true);
+            //canvas.SetActive(true);
             NewFieldManager.instance._panelOpen = true;
         }
     }
 
     public void DisablePanel()
     {
-        NewFieldManager.instance.fieldPlayerCanvas = canvas;
+        GameObject oldCanvas = Instantiate(newCanvas, canvas.transform.parent);
+        oldCanvas.SetActive(false);
+        Destroy(canvas);
+        NewFieldManager.instance.fieldPlayerCanvas = oldCanvas;
         NewFieldManager.instance._panelOpen = false;
         GetComponentInParent<PlayerController>().DisablePlayer(false);
-        canvas.SetActive(false);    
+        newCanvas.SetActive(false);
+        Destroy(newCanvas);
+
     }
 }
