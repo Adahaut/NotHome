a2d7d214a691ecec3c321f9d248efd474f4a50ef
lucasdprx <150352145+lucasdprx@users.noreply.github.com>
2024-05-29 11:33:28 +0200

    Decompression

--- NotHome/Assets/Scripts/QG/Decompression/DoorExit.cs
+++ NotHome/Assets/Scripts/QG/Decompression/DoorExit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorExit : MonoBehaviour
@@ -7,7 +8,8 @@ public class DoorExit : MonoBehaviour
     private int _nbPlayer;
     public static DoorExit Instance;
     private bool _qgIsLevel3;
-    [SerializeField] private ParticleSystem _smokeParticle;
+    private bool _isDecompression;
+    [SerializeField] private GameObject _smokeParticle;
     [SerializeField] private AudioSource _soundDecompression;
     private void Awake()
     {
@@ -22,30 +24,44 @@ public class DoorExit : MonoBehaviour
     {
         if (other.CompareTag("Player") && !_qgIsLevel3)
         {
-            print("enter");
             _nbPlayer++;
-            if (_nbPlayer >= 1)
-            {
-                _soundDecompression.Play();
-                _smokeParticle.Play();
-                _doorEnter.SetActive(true);
-                _doorExit.SetActive(false);
-            }
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") && !_qgIsLevel3)
         {
+            _smokeParticle.SetActive(false);
             _doorEnter.SetActive(false);
             _doorExit.SetActive(true);
             _nbPlayer--;
+            _isDecompression = false;
         }
     }
     public void QGLevel3()
     {
         _qgIsLevel3 = true;
         _doorEnter.SetActive(false);
+        _doorExit.SetActive(false);
+    }
+    public void OpenDoor(Transform camera)
+    {
+        if (Physics.Raycast(camera.position, camera.forward, out RaycastHit hit, PC.Instance.GetDistRayCast()))
+        {
+            if (hit.collider.CompareTag("Decompression") && _nbPlayer >= 1 && !_isDecompression)
+            {
+                _isDecompression = true;
+                _doorEnter.SetActive(true);
+                StartCoroutine(StartParticle(1));
+            }
+        }
+    }
+    private IEnumerator StartParticle(float second)
+    {
+        yield return new WaitForSeconds(second);
+        _soundDecompression.Play();
+        _smokeParticle.SetActive(true);
+        yield return new WaitForSeconds(_soundDecompression.clip.length - 1);
         _doorExit.SetActive(false);
     }
 }
