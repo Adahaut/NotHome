ce8c2f87f13476036c553a6a0e8bdb0faaee0ec1
Aurelien <aure.dugue@gmail.com>
2024-06-05 12:10:36 +0200

    finish camera deplacement when zooming

--- NotHome/Assets/Scripts/QG/Map/MapUI.cs
+++ NotHome/Assets/Scripts/QG/Map/MapUI.cs
@@ -14,11 +14,13 @@ public class MapUI : MonoBehaviour
     [SerializeField] private List<GameObject> _cameraDeplacementZone = new List<GameObject>();
 
     [SerializeField] private int _cameraMaxZoom, _cameraMinZoom;
-    private Vector3 _savedCameraPos;
     private Vector3 _dir;
     private int _currentMapIndex;
     private float _timer;
     bool _camActive = false;
+
+    private Vector3 _camZone;
+    private Vector3 _camLastPos;
 
     Vector3 _initialMousePos;
 
@@ -78,40 +80,60 @@ public class MapUI : MonoBehaviour
         if (!(_currentMapIndex >= 0 && _currentMapIndex < 4) || _timer > 0)
             return;
 
-        _timer = 0.05f;
+        _timer = 0.01f;
         Camera _currentCamera = _minimapsCamera[_currentMapIndex];
         float _scrollValue = ctx.ReadValue<Vector2>().y;
-        _scrollValue = _scrollValue > 0f ? -1f : 1f;
+        if(_scrollValue != 0)
+            _scrollValue = _scrollValue > 0f ? -1f : 1f;
 
-        if (_currentCamera.fieldOfView > _cameraMinZoom && _currentCamera.fieldOfView <= _cameraMaxZoom)
+        if (_currentCamera.fieldOfView >= _cameraMinZoom && _currentCamera.fieldOfView <= _cameraMaxZoom)
             _currentCamera.fieldOfView += _scrollValue;
             
 
         if (_currentCamera.fieldOfView < _cameraMinZoom)
         {
-            print("inferieur");
             _currentCamera.fieldOfView = _cameraMinZoom;
         }
 
         else if (_currentCamera.fieldOfView > _cameraMaxZoom)
         {
-            print("sup");
             _currentCamera.fieldOfView = _cameraMaxZoom;
         }
 
-        print(_currentCamera.fieldOfView);
-        AddCameraZoneScale(_scrollValue, _currentCamera);
+        CalculateCamDeplacementZone(_currentCamera.fieldOfView);
+        if (!CamIsInZone(_currentCamera.transform.localPosition))
+        {
+            if (_currentCamera.fieldOfView == _cameraMaxZoom)
+            {
+                _currentCamera.transform.localPosition = Vector3.zero;
+                return;
+            }
+
+            Vector2 _axeOut = CheckWichAxeIsOut(_currentCamera.transform.position);
+            Vector3 _newPos = _currentCamera.transform.localPosition;
+
+            if (_axeOut.x == -1)
+            {
+                _newPos.x = _camZone.x;
+            }
+            else if (_axeOut.x == 1)
+            {
+                _newPos.x = -(_camZone.x);
+            }
+
+            if (_axeOut.y == -1)
+            {
+                _newPos.z = _camZone.z;
+            }
+            else if (_axeOut.y == 1)
+            {
+                _newPos.z = -(_camZone.z);
+            }
+            _currentCamera.transform.localPosition = _newPos;
+        }
     }
 
 
-    private void AddCameraZoneScale(float _value, Camera _currentCamera)
-    {
-        if (_currentCamera.fieldOfView < _cameraMinZoom || _currentCamera.fieldOfView > _cameraMaxZoom)
-            return;
-
-        _cameraDeplacementZone[_currentMapIndex].transform.localScale = _cameraDeplacementZone[_currentMapIndex].transform.localScale - new Vector3(_value, _value, _value);
-
-    }
 
     private void Timer()
     {
@@ -146,33 +168,65 @@ public class MapUI : MonoBehaviour
             _initialMousePos = Vector3.zero;
             _dir = Vector3.zero;
         }
-        print("initial pos : " + _initialMousePos + " mouse current pos " + Input.mousePosition + " direction " + _dir);
-        Vector3 _camPos = _currentCamera.transform.position;
+        Vector3 _camPos = _currentCamera.transform.localPosition;
         if(_dir != Vector3.zero)
         {
-            _currentCamera.transform.position = new Vector3(_camPos.x - _dir.x, _camPos.y, _camPos.z - _dir.y);
+            _currentCamera.transform.localPosition = new Vector3(_camPos.x - _dir.x, _camPos.y, _camPos.z - _dir.y);
             _initialMousePos = Input.mousePosition;
             _dir = Vector3.zero;
-            _currentCamera.GetComponent<CameraMapLocker>()._lastPosition = _currentCamera.transform.position;
         }
+        if (!CamIsInZone(_currentCamera.transform.localPosition))
+        {
+            _currentCamera.transform.localPosition = _camLastPos;
+        }
+        _camLastPos = _currentCamera.transform.localPosition;
+    }
+
+    private void CalculateCamDeplacementZone(float _camFOV)
+    {
+        _camZone = 15f * new Vector3
+        {
+            x = _cameraMaxZoom - _camFOV,
+            y = 0,
+            z = _cameraMaxZoom - _camFOV
+        };
+    }
+
+    private bool CamIsInZone(Vector3 _camPosition)
+    {
+        return (_camPosition.x <= _camZone.x && _camPosition.x >= -(_camZone.x)) && (_camPosition.z <= _camZone.z && _camPosition.z >= -(_camZone.z));
+    }
+
+    private Vector2 CheckWichAxeIsOut(Vector3 _camPosition)
+    {
+        Vector2 _axeOut = Vector2.zero;
+        if (_camPosition.x > _camZone.x)
+            _axeOut.x = 1;
+        else if (_camPosition.x < -(_camZone.x))
+            _axeOut.x = -1;
+
+        if (_camPosition.z > _camZone.z)
+            _axeOut.y = 1;
+        else if (_camPosition.z < -(_camZone.z))
+            _axeOut.y = -1;
+
+        return _axeOut;
     }
 
     private void ResetCamera()
     {
         Camera _currentCamera = _minimapsCamera[_currentMapIndex];
         _currentCamera.fieldOfView = _cameraMaxZoom;
-        _currentCamera.transform.position = _savedCameraPos;
+        _currentCamera.transform.localPosition = Vector3.zero;
     }
 
     public void OpenMapByIndex(int _index)
     {
-        if (_savedCameraPos != Vector3.zero && (_currentMapIndex < _minimapsCamera.Count - 1))
+        if ((_currentMapIndex < _minimapsCamera.Count - 1))
             ResetCamera();
         DesactiveAllMaps();
         _mapsList[_index].SetActive(true);
         _currentMapIndex = _index;
-        if (_currentMapIndex < _minimapsCamera.Count - 1)
-            _savedCameraPos = _minimapsCamera[_currentMapIndex].transform.position;
         _camActive = true;
     }
 
