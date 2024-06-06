cd0252def5a33bda8665e15e0230eaf725e4f69d
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-06-06 15:32:45 +0200

    ENFIN

--- NotHome/Assets/Scripts/Cinematic/End/EndCinematic.cs
+++ NotHome/Assets/Scripts/Cinematic/End/EndCinematic.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class EndCinematic : MonoBehaviour
 {
     [SerializeField] private GameObject _spaceSkybox;
-    [SerializeField] private GameObject _meteorit;
+    [SerializeField] private GameObject _asteroid;
+    [SerializeField] private ParticleSystem[] _smoke;
+    [SerializeField] private GameObject[] _explosion;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void ActiveSpaceSkybox()
     {
         _spaceSkybox.SetActive(true);
     }
 
-    public void ActiveMeteorit()
+    public void ActiveAsteroid()
     {
-        _meteorit.SetActive(true);
+        _asteroid.SetActive(true);
+    }
+
+    public void PlaySmoke(int _index)
+    {
+        if (_smoke[_index].isPlaying)
+        {
+            _smoke[_index].Pause();
+        }
+        else
+        {
+            _smoke[_index].Play();
+        }
+    }
+
+    public void PlayExplosion(int _index)
+    {
+        ParticleSystem[] particleSystems = _explosion[_index].GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.Play();
+        }
     }
 }
