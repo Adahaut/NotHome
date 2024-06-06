d1ea8d9f3c4dfeb6bd19c3645f765225283ccdb0
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-06 13:38:41 +0200

    test update position seed

--- NotHome/Assets/Scripts/Multiplayer/SeedObject.cs
+++ NotHome/Assets/Scripts/Multiplayer/SeedObject.cs
@@ -8,16 +8,12 @@ public class SeedObject : NetworkBehaviour
 
     public Sprite seedImage;
     public Sprite fruitImage;
-
     
     [SyncVar] private bool _growStarted = false;
     [SyncVar] public float _timeToGrow;
-
     [SyncVar] public float _currentTimer;
 
-    private float yScale;
     private Vector3 initYPos;
-    private float posY;
 
     public void StartGrow(Vector3 pos)
     {
@@ -26,7 +22,6 @@ public class SeedObject : NetworkBehaviour
             _growStarted = true;
             _currentTimer = 0f;
             _timeToGrow = seedStruct._growingTime;
-            yScale = 0.25f;
 
             initYPos = pos;
         }
@@ -34,7 +29,7 @@ public class SeedObject : NetworkBehaviour
 
     private void Update()
     {
-        if (_growStarted)
+        if (_growStarted && isServer)
         {
             if (_currentTimer < _timeToGrow)
             {
@@ -47,13 +42,7 @@ public class SeedObject : NetworkBehaviour
                 Vector3 newScale = new Vector3(.2f, yScale, .2f);
                 transform.localScale = newScale;
 
-                //Update Position
-                float newYPos = Mathf.Lerp(initYPos.y, initYPos.y + 0.25f, t);
-
-                Vector3 newPos = transform.position;
-                newPos.y = newYPos;
-                CmdSendPositionToServer( newPos );
-                //transform.position = newPos;
+                RpcUpdateGrowthPosition(t);
             }
             else
             {
@@ -62,23 +51,17 @@ public class SeedObject : NetworkBehaviour
         }
     }
 
-    void CmdSendPositionToServer(Vector3 position)
-    {
-        // Mettre � jour la position du joueur sur le serveur
-        transform.position = position;
-
-        // Envoyer la position mise � jour � tous les clients
-        RpcUpdatePositionOnClients(position);
-    }
-
     [ClientRpc]
-    void RpcUpdatePositionOnClients(Vector3 position)
+    private void RpcUpdateGrowthPosition(float t)
     {
-        if (!isOwned)
-        {
-            // Mettre � jour la position du joueur sur les clients
-            transform.position = position;
-        }
+        print("caca");
+
+        //Update Position
+        float newYPos = Mathf.Lerp(initYPos.y, initYPos.y + 0.25f, t);
+
+        Vector3 newPos = transform.position;
+        newPos.y = newYPos;
+        transform.position = newPos;
     }
 
 }
