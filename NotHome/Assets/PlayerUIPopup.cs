96d896737744eebe72f66b2210abcded68f972c0
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-07 09:58:52 +0200

    Add anim + sound on quest notification + test for multiplayer

--- NotHome/Assets/PlayerUIPopup.cs
+++ NotHome/Assets/PlayerUIPopup.cs
@@ -10,11 +10,14 @@ public class PlayerUIPopup : NetworkBehaviour
     [SerializeField] private GameObject popup;
     [SerializeField] private Animator popupAnimator;
     [SerializeField] private TMP_Text titleQuestCompleted;
+    [SerializeField] private AudioSource notificationSound;
 
     public void PopUpQuestAchieve(string text)
     {
         popup.SetActive(true);
+        popupAnimator.SetBool("OpenNotification", true);
         titleQuestCompleted.text = text;
+        notificationSound.Play();
         StartCoroutine(Tempcoroutine());
     }
 
@@ -24,15 +27,25 @@ public class PlayerUIPopup : NetworkBehaviour
         NetworkClient.RegisterHandler<QuestNotificationMessage>(OnClientReceiveMessage);
     }
 
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        NetworkServer.RegisterHandler<QuestNotificationMessage>(OnServerReceiveMessage);
+    }
+
     void OnClientReceiveMessage(QuestNotificationMessage msg)
     {
         PopUpQuestAchieve(msg.title);
-        Debug.Log("caca");
+    }
+
+    void OnServerReceiveMessage(NetworkConnection conn, QuestNotificationMessage msg)
+    {
+        PopUpQuestAchieve(msg.title);
     }
 
     IEnumerator Tempcoroutine()
     {
-        yield return new WaitForSeconds(3f);
-        popup.SetActive(false);
+        yield return new WaitForSeconds(3.5f);
+        popupAnimator.SetBool("OpenNotification", false);
     }
 }
