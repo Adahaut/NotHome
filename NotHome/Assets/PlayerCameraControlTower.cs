0d156ece46c84949e0620d6573c2a10dbdd0b716
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-07 11:57:41 +0200

    Update PlayerCameraControlTower.cs

--- NotHome/Assets/PlayerCameraControlTower.cs
+++ NotHome/Assets/PlayerCameraControlTower.cs
@@ -1,38 +1,38 @@
-using Mirror;
-using Unity.VisualScripting;
-using UnityEngine;
+//using Mirror;
+//using Unity.VisualScripting;
+//using UnityEngine;
 
-public class PlayerCameraControlTower : NetworkBehaviour
-{
+//public class PlayerCameraControlTower : NetworkBehaviour
+//{
 
-    public RenderTexture[] renderTextures;
+//    public RenderTexture[] renderTextures;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            //other.gameObject.GetComponentInChildren<FollowCamera>().linkedCam.enabled = false;
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if(other.CompareTag("Player"))
+//        {
+//            //other.gameObject.GetComponentInChildren<FollowCamera>().linkedCam.enabled = false;
+//            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-            for (int i = 0; i < players.Length; i++)
-            {
-                if (players[i] != other.gameObject)
-                    players[i].GetComponent<PlayerCameraManager>().playerRenderCamera.targetTexture = renderTextures[i];
-            }
-        }
-    }
+//            for (int i = 0; i < players.Length; i++)
+//            {
+//                if (players[i] != other.gameObject)
+//                    players[i].GetComponent<PlayerCameraManager>().playerRenderCamera.targetTexture = renderTextures[i];
+//            }
+//        }
+//    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-            for (int i = 0; i < players.Length; i++)
-            {
-                if (players[i] != other.gameObject)
-                    players[i].GetComponent<PlayerCameraManager>().playerRenderCamera.targetTexture = null;
-            }
-        }
-    }
-}
+//            for (int i = 0; i < players.Length; i++)
+//            {
+//                if (players[i] != other.gameObject)
+//                    players[i].GetComponent<PlayerCameraManager>().playerRenderCamera.targetTexture = null;
+//            }
+//        }
+//    }
+//}
