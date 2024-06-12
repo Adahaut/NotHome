2f2ed8d821f98cc3a28cfa8b0341929021b31fca
Aurelien <aure.dugue@gmail.com>
2024-06-12 14:23:06 +0200

    outline Objects

---
+++ NotHome/Assets/Scripts/Player/OutlineSelector.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OutlineSelector : MonoBehaviour
+{
+    private Transform _hightlight;
+    private Transform _selection;
+    private RaycastHit _hit;
+    private Camera _playerCam;
+    private Transform _cameraTransform;
+    [SerializeField] private Color _outlineColor;
+    [SerializeField] private float _outlineWidth;
+    [SerializeField] private float _distance;
+
+    private void Start()
+    {
+        _playerCam = GetComponentInChildren<Camera>();
+        _cameraTransform = _playerCam.transform;
+    }
+
+    private void Update()
+    {
+        if(_hightlight != null)
+        {
+            _hightlight.gameObject.GetComponent<Outline>().enabled = false;
+            _hightlight = null;
+        }
+
+        if(!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(_cameraTransform.position, _cameraTransform.forward, out _hit, _distance))
+        {
+            _hightlight = _hit.transform;
+            if(_hightlight.CompareTag("Item") || _hightlight.CompareTag("Ladder") || _hightlight.gameObject.layer == LayerMask.NameToLayer("Interectable") || _hightlight.gameObject.layer == LayerMask.NameToLayer("Item"))
+            {
+                if(_hightlight.GetComponent<Outline>() != null)
+                {
+                    _hightlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = _hightlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    outline.OutlineColor = _outlineColor;
+                    outline.OutlineWidth = _outlineWidth;
+                }
+            }
+            else
+            {
+                _hightlight = null;
+            }
+
+        }
+
+    }
+}
