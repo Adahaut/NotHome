04f76a366121bf9d599607b3c889e5b787788ac5
Adahaut <adao.vo.t@gmail.com>
2024-06-13 14:56:35 +0200

    first try menu

--- NotHome/Assets/Scripts/UI/MainMenuManager.cs
+++ NotHome/Assets/Scripts/UI/MainMenuManager.cs
@@ -6,6 +6,9 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject _uiOption;
     [SerializeField] private GameObject _uiCredit;
     [SerializeField] private GameObject _uiMainMenu;
+    [SerializeField] private GameObject _camera;
+
+    private float _camY = 0;
     public void PlayGame()
     {
         SceneManager.LoadScene("FixGame");
@@ -23,5 +26,16 @@ public class MainMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void rotatecam()
+    {
+        _camY += 0.01f;
+        _camera.transform.rotation = Quaternion.Euler(0, 0, _camY);
+    }
+
+    private void Update()
+    {
+        rotatecam();
     }
 }
