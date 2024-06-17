0ce08fc224949695e8644d1432ed72308eb07f07
Adahaut <adao.vo.t@gmail.com>
2024-06-17 16:14:48 +0200

    Update PlayerDeathAndRespawn.cs

--- NotHome/Assets/Scripts/Player/PlayerDeathAndRespawn.cs
+++ NotHome/Assets/Scripts/Player/PlayerDeathAndRespawn.cs
@@ -69,6 +69,7 @@ public class PlayerDeathAndRespawn : NetworkBehaviour
             {
                 time += Time.deltaTime;
                 cameraTransform.position = Vector3.Lerp(_initCamPos, _initCamPos + new Vector3(2, -1.5f, 2), time / totalTime * 3f);
+                cameraTransform.rotation = Quaternion.Euler(cameraTransform.rotation.x, cameraTransform.rotation.y + (360*time/totalTime),cameraTransform.rotation.z) ;
                 yield return new WaitForEndOfFrame();
             }
 
