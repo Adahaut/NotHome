ef09f7c1e071ffbdaa02a2d5758c93d700190097
Aurelien <aure.dugue@gmail.com>
2024-06-19 14:45:33 +0200

    add image for crafts

--- NotHome/Assets/Scripts/QG/Craft/CraftScriptableObject.cs
+++ NotHome/Assets/Scripts/QG/Craft/CraftScriptableObject.cs
@@ -6,6 +6,7 @@ public class CraftScriptableObject : ScriptableObject
 {
     public List<string> _materialName = new List<string>();
     public List<int> _materialNumber = new List<int>();
+    public List<Sprite> _materialSprite = new List<Sprite>();
 
     public string _resultName;
     public Sprite _resultSprite;
