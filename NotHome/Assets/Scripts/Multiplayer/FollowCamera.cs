ab90b58b2baaadea27513037297490f732522e64
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-20 12:18:29 +0200

    Update FollowCamera.cs

--- NotHome/Assets/Scripts/Multiplayer/FollowCamera.cs
+++ NotHome/Assets/Scripts/Multiplayer/FollowCamera.cs
@@ -7,9 +7,13 @@ public class FollowCamera : MonoBehaviour
     public Camera linkedCam;
 
     public Camera cam;
+    Vector3 newRotation;
     private void Update()
     {
         linkedCam.transform.position = cam.transform.position;
-        linkedCam.transform.rotation = cam.transform.rotation;
+
+        newRotation = new Vector3(cam.transform.rotation.x, transform.root.rotation.y, cam.transform.rotation.z);
+
+        linkedCam.transform.rotation = Quaternion.Euler(newRotation);
     }
 }
