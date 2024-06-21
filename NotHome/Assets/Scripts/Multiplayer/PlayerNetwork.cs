87e0a26816d3accac6a562a4210602422084dd24
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-21 10:33:04 +0200

    Update PlayerNetwork.cs

--- NotHome/Assets/Scripts/Multiplayer/PlayerNetwork.cs
+++ NotHome/Assets/Scripts/Multiplayer/PlayerNetwork.cs
@@ -62,10 +62,10 @@ public class PlayerNetwork : NetworkBehaviour
         {
             foreach (var playerCamera in _playerCameras)
             {
-                if (playerCamera.transform.root.GetComponent<PlayerNetwork>().nameTagText.text == "-")
-                {
-                    playerCamera.transform.root.GetComponent<PlayerNetwork>().nameTagText.text = playerCamera.transform.root.GetComponent<PlayerNetwork>()._displayName;
-                }
+                //if (playerCamera.transform.root.GetComponent<PlayerNetwork>().nameTagText.text == "-")
+                //{
+                //    playerCamera.transform.root.GetComponent<PlayerNetwork>().nameTagText.text = playerCamera.transform.root.GetComponent<PlayerNetwork>()._displayName;
+                //}
 
                 nameTagInstance.transform.LookAt(playerCamera.transform);
                 nameTagInstance.transform.Rotate(0, 180, 0);
