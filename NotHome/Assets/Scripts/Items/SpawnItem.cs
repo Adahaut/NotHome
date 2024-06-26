fdfc841fe7d6cb3b16442699edb04c5b30e082d7
Elvin-04 <118186018+Elvin-04@users.noreply.github.com>
2024-06-26 10:53:32 +0200

    Update SpawnItem.cs

--- NotHome/Assets/Scripts/Items/SpawnItem.cs
+++ NotHome/Assets/Scripts/Items/SpawnItem.cs
@@ -1,8 +1,6 @@
 using Mirror;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
-using UnityEngine.UIElements;
 
 public class SpawnItem : NetworkBehaviour
 {
